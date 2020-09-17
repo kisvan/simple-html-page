81e83ec27173c7af69776424d9e0c9a2f345955a
kisvan <65876227+kisvan@users.noreply.github.com>
2020-09-17 13:25:07 +0300

    Add files via upload

---
+++ project1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>kisvan demo page</title>
+	 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+
+</head>
+<body>
+	<div class="container">
+  <h1>WEBSITE DEVELOPMENT PROJECT</h1>
+  <p>BOARD OF WEB DEVELOPMENT TEMPLATES</p>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>From the scratch</h3>
+      <p>Here the start list..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>In progress</h3>
+      <p>Here are progress list..</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Completion</h3>
+      <p>Project completed</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
