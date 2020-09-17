d35cadaba5dcf82b5d77b88b2a7829d4195407df
kisvan <65876227+kisvan@users.noreply.github.com>
2020-09-17 13:14:18 +0300

    Add files via upload

---
+++ company1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kisvan demo page</title>
+	 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  
+
+</head>
+
+</head>
+<body>
+<div class="jumbotron text-center">
+  <h1>KISVAN'S WEB DEVELOPMENT</h1>
+  <p>We specialize in website design</p>
+  <form class="form-inline">
+    <div class="input-group">
+      <input type="email" class="form-control" size="50" placeholder="Email Address" required>
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-danger">Subscribe</button>
+      </div>
+    </div>
+  </form>
+</div>
+</body>
+</html>
